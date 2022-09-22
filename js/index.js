0dc9c0d58a41111d7d60bc15cccd13cb978ab942
poiega <110189813+poiega@users.noreply.github.com>
2022-09-22 18:45:49 +0900

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,41 @@
+console.log(
+            '%c잠깐만요!', 
+            'color:RED; font-size:35px;'
+        );
+        console.log('지금 뭘 하려는진 모르겠지만.. 그대로 X버튼을 눌러주세요')
+        // function doSomething() {
+        //     $('#title_desc2').css({
+        //         'animation':'intro3 2s',
+        //         'animation-fill-mode': 'forwards'
+        //     })
+        //     $('#title_h3').css({
+        //         'animation':'intro3 2s',
+        //         'animation-fill-mode': 'forwards'
+        //     })
+        //     $('#img_1').css({
+        //         'animation':'intro3 2s',
+        //         'animation-fill-mode': 'forwards'
+        //     })
+        //     $('#invite_btn').css({
+        //         'animation':'intro3 2s',
+        //         'animation-fill-mode': 'forwards'
+        //     })
+        //     $('#server_btn').css({
+        //         'animation':'intro3 2s',
+        //         'animation-fill-mode': 'forwards'
+        //     })
+        // }
+         
+        // $(function () {
+         
+        //     $(window).scroll( function() {
+        //         doSomething(); 
+        //     });
+         
+        // });
+        function invite_btn(){
+            window.location.href = "https://discord.com/oauth2/authorize?client_id=1010940350824726548&scope=bot"
+        }
+        function invite_btn2(){
+            window.location.href = "https://discord.gg/JuCZkpAyrb"
+        }
